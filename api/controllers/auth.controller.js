5c849292f605ef9abbed59a026cb76cd357aa4ef
Koushik Das <90111866+Dev-KoushikDas@users.noreply.github.com>
2023-05-05 23:32:32 +0530

    Update auth.controller.js

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -49,7 +49,7 @@ export const login = async (req, res, next) => {
     id: user._id,
     isSeller: user.isSeller,
     },
-    process.env.JWT_KEY
+    process.env.JWTKEY
     );
 
     const { password, ...info } = user._doc;
