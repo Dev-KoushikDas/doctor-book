b4794ad0e49c1aef35401d8db103a9ceb44a8f14
Koushik Das <90111866+Dev-KoushikDas@users.noreply.github.com>
2023-05-06 02:10:52 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ const connect = async () => {
   }
 };
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://doctor-book-rq6v.vercel.app/", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
