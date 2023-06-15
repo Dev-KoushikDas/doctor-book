dfdf49e1a9c6d66adee4362983635ec2e00f86c3
Dev-KoushikDas <das.koushik2609@gmail.com>
2023-06-15 16:04:29 +0530

    com

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ const connect = async () => {
   }
 };
 
-app.use(cors({ origin: "https://doctor-book-rq6v.vercel.app/", credentials: true }));
+app.use(cors({ origin: "https://doctor-book-rq6v.vercel.app", credentials: true }));
 app.use(express.json());
 app.use(cookieparser());
 
