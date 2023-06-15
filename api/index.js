1fc3a24318264b67554f23f14e70e404e62bb5b7
Koushik Das <90111866+Dev-KoushikDas@users.noreply.github.com>
2023-06-15 17:08:52 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -43,7 +43,7 @@ app.use((err, req, res, next) => {
   return res.status(errorStatus).send(errorMessage);
 });
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   connect();
   console.log("Backend server is running!");
 });
