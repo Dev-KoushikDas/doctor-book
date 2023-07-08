5906563f473543a2e9bc6b7962c572f7f64a44c3
Dev-KoushikDas <das.koushik2609@gmail.com>
2023-07-08 22:46:53 +0530

    stripe2

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ const connect = async () => {
   }
 };
 
-app.use(cors({ origin:"*", credentials: true }));
+app.use(cors({ origin:true, credentials: true }));
 app.use(express.json());
 app.use(cookieparser());
 
