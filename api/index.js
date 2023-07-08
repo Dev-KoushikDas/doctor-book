443592d149eff912be10e4f0471738ca9bf21fce
Dev-KoushikDas <das.koushik2609@gmail.com>
2023-07-08 21:17:03 +0530

    change2

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ const connect = async () => {
   }
 };
 
-app.use(cors({ origin:"http://localhost:3000", credentials: true }));
+app.use(cors({ origin:"https://doctor-book.vercel.app/", credentials: true }));
 app.use(express.json());
 app.use(cookieparser());
 
