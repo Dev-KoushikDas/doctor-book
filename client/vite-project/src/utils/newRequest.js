0a84e2e810344ff46f81e11be7fa335d70e5eff6
Dev-KoushikDas <das.koushik2609@gmail.com>
2023-07-09 13:01:42 +0530

    new2

--- client/vite-project/src/utils/newRequest.js
+++ client/vite-project/src/utils/newRequest.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "https://vercel.com/dev-koushikdas/doctor-book-b4t2/",
-//  withCredentials: true,
+  baseURL: "https://vercel.com/dev-koushikdas/doctor-book-b4t2/api",
+  withCredentials: true,
  // origin:"*"
 });
 
